1bf0a196c0aaac3c6634f5f4f6594b3216a1e9f0
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-02-04 21:49:32 +0530

    Many changes - added getMin_team_no, selSort_team_no and writeToFile functions - Added main logic for selection sort - Converted doubly linked list to singly linked list - Updated with comments - added gitignore to ignore input and output text files

--- lab_1/opt_sort.c
+++ lab_1/opt_sort.c
@@ -9,13 +9,13 @@
 #include<stdlib.h>
 #include<string.h>
 
+// Singly linked list
 struct student
 {
 	char *ht_no;
 	char *name;
 	int team_no;
 
-	struct student *prev;
 	struct student *next;
 };
 
@@ -23,35 +23,42 @@ typedef struct student student;
 
 int main()
 {
+	// Function prototypes
 	student *getLinkedList(FILE*);
+	student *getMin_team_no(student*);
+	void selSort_team_no(student*);
 	void swap(student*, student*);
+	void writeToFile(FILE*, student*);
 
-	FILE *inFPtr = NULL;
+	// File I/O
+	FILE *inFPtr = NULL, *outFPtr;
 	
-	inFPtr = fopen("DAALab_Input1.txt", "r");
+	inFPtr = fopen("DAALab_input1.txt", "r");
+	outFPtr = fopen("DAALab_output1.txt", "w");
 
 	if(inFPtr == NULL)
 	{
-		fprintf(stderr, "File 'DAALab_Input1.txt' doesn't exist, exiting...");
+		fprintf(stderr, "File 'DAALab_input1.txt' doesn't exist, exiting...");
 		return -1;
 	}
+	// End of file I/O
 
+	// Making a linked list from the file
 	student *root = getLinkedList(inFPtr);
 	fclose(inFPtr);
 
-	student *temp = root;
+	// Da sorting. TODO: change to command line args
+	selSort_team_no(root);
 
-	swap(root, root -> next);
-
-	while (temp != NULL)
-	{
-		printf("Person: '%s' '%s' %d\n", temp -> ht_no, temp -> name, temp -> team_no);
-		temp = temp -> next;
-	}
+	// Write the sorted list into file
+	writeToFile(outFPtr, root);
+	fclose(outFPtr);
 
 	return(0);
 }
 
+
+// Function to get a linked list from the contents of the input file
 student *getLinkedList(FILE *inFPtr)
 {
 	student *head = NULL, *temp, *new_student;
@@ -84,16 +91,10 @@ student *getLinkedList(FILE *inFPtr)
 				// Adding the new node to the linked list
 
 				if(head == NULL)
-				{
 					head = new_student;
-					head -> prev = NULL;
-				}
 
 				else
-				{
 					temp -> next = new_student;
-					new_student -> prev = temp;
-				}
 
 				temp = new_student;
 				temp -> next = NULL;
@@ -104,7 +105,7 @@ student *getLinkedList(FILE *inFPtr)
 	return head;
 }
 
-// Function to swap the whole node, instead of copying values
+// Function to swap the values inside the node efficiently
 void swap(student *node_1, student *node_2)
 {
 	char *temp_ht_no = node_1 -> ht_no, *temp_name = node_1 -> name;
@@ -117,4 +118,49 @@ void swap(student *node_1, student *node_2)
 	node_2 -> ht_no = temp_ht_no;
 	node_2 -> name = temp_name;
 	node_2 -> team_no = temp_team_no;
+}
+
+// Function to get the address of the node having the minimum value
+student* getMin_team_no(student* start)
+{
+	student *min_student, *temp;
+
+	min_student = temp = start;
+
+	while(temp != NULL)
+	{
+		if((temp -> team_no) < (min_student -> team_no))
+			min_student = temp;
+
+		temp = temp -> next;
+	}
+
+	return min_student;
+}
+
+// Selection sort based on based on team number
+void selSort_team_no(student* root)
+{
+	student *temp = root, *hulu;
+
+	while(temp != NULL)
+	{
+		if(temp -> next != NULL)
+			swap(temp, getMin_team_no(temp));
+		
+		temp = temp -> next;
+	}
+}
+
+// Function to write the linked list into the output file
+void writeToFile(FILE *outFPtr, student *root)
+{
+	student *temp = root;
+
+	while(temp != NULL)
+	{
+		fprintf(outFPtr, "%s %s %d\n", temp -> ht_no, temp -> name, temp -> team_no);
+		
+		temp = temp -> next;
+	}
 }
