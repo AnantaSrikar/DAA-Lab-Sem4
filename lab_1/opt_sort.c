254c56293c996e8db7bba357d45f0a3b6383ab73
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2021-02-05 13:17:19 +0530

    Added insertion sort - made the whole program more compact - and also structured command line args

--- lab_1/opt_sort.c
+++ lab_1/opt_sort.c
@@ -8,6 +8,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 
 // Singly linked list
 struct student
@@ -21,13 +22,45 @@ struct student
 
 typedef struct student student;
 
-int main()
+int main(int argc, char **argv)
 {
+	// Initial code to get all command line values
+
+	if(argc != 3)
+	{
+		printf("Wrong number of arguments. Please go through README.md");
+		return -1;
+	}
+
+	for(int i = 1; i <= 2; i++)
+	{
+		for(int j = 0; j < strlen(argv[i]); j++)
+			if(!isdigit(argv[i][j]))
+			{
+				printf("Invalid arguments! Please go through README.md"); // Enter only numbers!
+				return -1;
+			}
+	}
+
+	int sort_meth = atoi(argv[1]);
+	int field = atoi(argv[2]);
+
+	if(!(sort_meth == 1 || sort_meth == 2) || !(field >= 1 && field <= 3))
+	{
+		printf("Invalid numbers entered. Please go through README.md");
+		return -1;
+	}
+
+	// End of command line arguments
+
+
 	// Function prototypes
 	student *getLinkedList(FILE*);
 	student *getMin_team_no(student*);
-	void selSort_team_no(student*);
-	void swap(student*, student*);
+
+	void selSort(student*, int);
+	void insSort(student*, int);
+	
 	void writeToFile(FILE*, student*);
 
 	// File I/O
@@ -47,8 +80,20 @@ int main()
 	student *root = getLinkedList(inFPtr);
 	fclose(inFPtr);
 
-	// Da sorting. TODO: change to command line args
-	selSort_team_no(root);
+	// switch to sort according to user input
+	switch(sort_meth)
+	{
+	case 1:
+			selSort(root, field);
+			break;
+
+	case 2:
+			insSort(root, field);
+			break;
+
+	default:
+			printf("Something bad happened :(\n");
+	}
 
 	// Write the sorted list into file
 	writeToFile(outFPtr, root);
@@ -120,8 +165,8 @@ void swap(student *node_1, student *node_2)
 	node_2 -> team_no = temp_team_no;
 }
 
-// Function to get the address of the node having the minimum value
-student* getMin_team_no(student* start)
+// Function to get the address of the node having the minimum value wrt a field
+student* getMinStudent(student* start, int field)
 {
 	student *min_student, *temp;
 
@@ -129,8 +174,26 @@ student* getMin_team_no(student* start)
 
 	while(temp != NULL)
 	{
-		if((temp -> team_no) < (min_student -> team_no))
-			min_student = temp;
+		switch(field)
+		{
+		case 1:
+				if(strcmp(temp -> ht_no, min_student -> ht_no) < 0)
+					min_student = temp;
+				break;
+
+		case 2:
+				if(strcmp(temp -> name, min_student -> name) < 0)
+					min_student = temp;
+				break;
+
+		case 3:
+				if((temp -> team_no) < (min_student -> team_no))
+					min_student = temp;
+				break;
+		
+		default:
+				printf("Something bad happened!!\n");
+		}
 
 		temp = temp -> next;
 	}
@@ -138,19 +201,149 @@ student* getMin_team_no(student* start)
 	return min_student;
 }
 
-// Selection sort based on based on team number
-void selSort_team_no(student* root)
+/*
+	Following is the mapping done for fields for both the sorting methods
+	1 - Hall Ticket number
+	2 - Name
+	3 - Team number
+*/
+
+// Selection sort for all three fields
+void selSort(student* root, int field)
 {
-	student *temp = root, *hulu;
+	student *temp = root;
 
 	while(temp != NULL)
 	{
 		if(temp -> next != NULL)
-			swap(temp, getMin_team_no(temp));
+			swap(temp, getMinStudent(temp, field));
 		
 		temp = temp -> next;
 	}
 }
+
+// Instertion sort for all three fields
+void insSort(student *root, int field)
+{
+	if(root == NULL || root -> next == NULL)
+		return;
+
+	student *temp_1 = root -> next;
+
+	while(temp_1 != NULL)
+	{
+		student *temp_2 = root;
+
+		switch(field)
+		{
+			case 1:
+			{
+				char *search_val = temp_1 -> ht_no;
+				int found = 0;
+
+
+				while(temp_2 != temp_1)
+				{
+					if(strcmp(temp_1 -> ht_no, temp_2 -> ht_no) < 0 && found == 0)
+					{
+						search_val = temp_2 -> ht_no;
+						temp_2 -> ht_no = temp_1 -> ht_no;
+						found = 1;
+						temp_2 = temp_2 -> next;
+					}
+					
+					else
+					{
+						if(found == 1)
+						{
+							char *temp = search_val;
+							search_val = temp_2 -> ht_no;
+							temp_2 -> ht_no = temp;
+						}
+
+						temp_2 = temp_2 -> next;
+					}
+				}
+				temp_2 -> ht_no = search_val;
+				temp_1 = temp_1 -> next;
+
+				break;
+			}
+
+			case 2:
+			{
+				char *search_val = temp_1 -> name;
+				int found = 0;
+
+
+				while(temp_2 != temp_1)
+				{
+					if(strcmp(temp_1 -> name, temp_2 -> name) < 0 && found == 0)
+					{
+						search_val = temp_2 -> name;
+						temp_2 -> name = temp_1 -> name;
+						found = 1;
+						temp_2 = temp_2 -> next;
+					}
+					
+					else
+					{
+						if(found == 1)
+						{
+							char *temp = search_val;
+							search_val = temp_2 -> name;
+							temp_2 -> name = temp;
+						}
+
+						temp_2 = temp_2 -> next;
+					}
+				}
+				temp_2 -> name = search_val;
+				temp_1 = temp_1 -> next;
+
+				break;
+			}
+
+			case 3:
+			{
+				int search_val = temp_1 -> team_no;
+				int found = 0;
+
+
+				while(temp_2 != temp_1)
+				{
+					if(temp_2 -> team_no > temp_1 -> team_no && found == 0)
+					{
+						search_val = temp_2 -> team_no;
+						temp_2 -> team_no = temp_1 -> team_no;
+						found = 1;
+						temp_2 = temp_2 -> next;
+					}
+					
+					else
+					{
+						if(found == 1)
+						{
+							int temp = search_val;
+							search_val = temp_2 -> team_no;
+							temp_2 -> team_no = temp;
+						}
+
+						temp_2 = temp_2 -> next;
+					}
+				}
+				temp_2 -> team_no = search_val;
+				temp_1 = temp_1 -> next;
+
+				break;
+			}
+			
+			default:
+				break;
+		}
+	}
+}
+
 
 // Function to write the linked list into the output file
 void writeToFile(FILE *outFPtr, student *root)
